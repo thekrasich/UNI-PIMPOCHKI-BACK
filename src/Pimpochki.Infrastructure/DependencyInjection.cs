95565a30109cfc90cd93535bccdf445664690a83
RillSvai <kirilkvas07@gmail.com>
2024-03-12 16:41:48 +0200

    feature: add connection string to production database

--- src/Pimpochki.Infrastructure/DependencyInjection.cs
+++ src/Pimpochki.Infrastructure/DependencyInjection.cs
@@ -4,8 +4,9 @@ namespace Pimpochki.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string databaseConnectionString)
     {
+        // Add DbContext
         return services;
     }
 }
