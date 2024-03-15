63bf23d770382ffa4f51b19bfe60b970c476c885
Myroslav <muroslavkl@gmail.com>
2024-03-15 15:48:49 +0200

    Fix: change registration file in Infrastructure project to migrate correctly

--- src/Pimpochki.Infrastructure/DependencyInjection.cs
+++ src/Pimpochki.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pimpochki.Infrastructure.Persistence;
 
@@ -6,9 +7,11 @@ namespace Pimpochki.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string databaseConnectionString)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<PimpochkiDbContext>(options => options.UseSqlServer(databaseConnectionString,b => b.MigrationsAssembly("Pimpochki.Api")));
+        services.AddDbContext<PimpochkiDbContext>(options => {
+            options.UseSqlServer(configuration.GetConnectionString("Database"));
+        });
         return services;
     }
 }
