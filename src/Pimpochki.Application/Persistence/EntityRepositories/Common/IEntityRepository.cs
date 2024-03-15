82479426b0859860bf6f4ef2ce717def0ca014a2
Myroslav <muroslavkl@gmail.com>
2024-03-15 18:39:53 +0200

    feature: add base repository Interface

---
+++ src/Pimpochki.Application/Persistence/EntityRepositories/Common/IEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pimpochki.Domain.Entities.Common;
+
+namespace Pimpochki.Application.Persistence.EntityRepositories.Common;
+
+public interface IEntityRepository<TEntity> where TEntity:class
+{
+    Task<List<TEntity>> GetAllAsync();
+    Task<TEntity> GetByIdAsync(int id);
+    Task InsertAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+
+}
