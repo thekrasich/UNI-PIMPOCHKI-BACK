2b5d4104d8ce596d936ea9845f8a7ded85554b9b
Myroslav <muroslavkl@gmail.com>
2024-03-15 19:10:58 +0200

    Feature: implement base repository interface

--- src/Pimpochki.Application/Persistence/EntityRepositories/Common/IEntityRepository.cs
+++ src/Pimpochki.Application/Persistence/EntityRepositories/Common/IEntityRepository.cs
@@ -1,13 +1,17 @@
-﻿using Pimpochki.Domain.Entities.Common;
+﻿using System.Linq.Expressions;
+using Pimpochki.Domain.Entities.Common;
 
 namespace Pimpochki.Application.Persistence.EntityRepositories.Common;
 
-public interface IEntityRepository<TEntity> where TEntity:class
+public interface IEntityRepository<TEntity> where TEntity:EntityBase
 {
-    Task<List<TEntity>> GetAllAsync();
-    Task<TEntity> GetByIdAsync(int id);
+    IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "");
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "", bool isTracked = false);
+    TEntity? Get(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "", bool isTracked = false);
     Task InsertAsync(TEntity entity);
+    Task InsertRangeAsync(IEnumerable<TEntity> entities);
     void Update(TEntity entity);
     void Delete(TEntity entity);
+    void DeleteRange(IEnumerable<TEntity> entities);
 
 }
---
+++ src/Pimpochki.Infrastructure/Persistence/EntityRepositories/Common/EntityRepository.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Pimpochki.Application.Persistence.EntityRepositories.Common;
+using Pimpochki.Domain.Entities.Common;
+
+namespace Pimpochki.Infrastructure.Persistence.EntityRepositories.Common;
+
+public class EntityRepository<TEntity>:IEntityRepository<TEntity> where TEntity: EntityBase
+{
+     private readonly PimpochkiDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public EntityRepository(PimpochkiDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+        public virtual void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public TEntity? Get(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "", bool isTracked = false)
+        {
+            IQueryable<TEntity> query = isTracked ? _dbSet : _dbSet.AsNoTracking();
+            if (filter is not null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (string includeProperty in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            return query.FirstOrDefault();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _dbSet;
+            if (filter is not null) 
+            {
+                query = query.Where(filter);
+            }
+            foreach (string includeProperty in includeProperties.Split(',',StringSplitOptions.RemoveEmptyEntries)) 
+            {
+                query = query.Include(includeProperty);
+            }
+            return query;
+        }
+
+        public virtual async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "", bool isTracked = false)
+        {
+            IQueryable<TEntity> query = isTracked ? _dbSet : _dbSet.AsNoTracking();
+            if (filter is not null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (string includeProperty in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public virtual async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task InsertRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+    
+}
