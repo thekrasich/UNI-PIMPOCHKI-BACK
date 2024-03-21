620458f6593de080347f78b881d82075118d537b
Myroslav <muroslavkl@gmail.com>
2024-03-21 17:23:47 +0200

    feature: add OrderController.cs

--- src/Pimpochki.Api/Controllers/OrderController.cs
+++ src/Pimpochki.Api/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pimpochki.Application.Dtos.OrderDtos;
+using Pimpochki.Application.Persistence.EntityServices;
 
 namespace Pimpochki.Api.Controllers
 {
@@ -7,5 +9,18 @@ namespace Pimpochki.Api.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<OrderDto> GetOrder([FromRoute] int id)
+        {
+            var order =  await _orderService.GetOrder(id);
+            return order;
+        }
     }
 }
