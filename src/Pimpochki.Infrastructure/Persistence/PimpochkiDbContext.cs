ce8e2f6246525272851b0c934d4b768438384759
RillSvai <kirilkvas07@gmail.com>
2024-04-02 11:14:21 +0300

    feature: add template for seeding products

--- src/Pimpochki.Infrastructure/Persistence/PimpochkiDbContext.cs
+++ src/Pimpochki.Infrastructure/Persistence/PimpochkiDbContext.cs
@@ -12,11 +12,32 @@ public class PimpochkiDbContext:DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        Image[] images =
+        [
+            new Image()
+            {
+                Id = 1,
+                ProductId = 1,
+                Url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwMGpJzhZ-k2WSmpUGDpOFg57h6-vJbb7oBA&s"
+            }
+        ];
+        Product[] products =
+        [
+            new Product()
+            {
+                Id = 1,
+                Description = "ERERER",
+                Quantity = 3,
+            }];
+        
         modelBuilder.Entity<Role>().HasData([
             new Role {Id = 1, UserRole = Application.Enums.Role.Customer.ToString()},
             new Role {Id = 2, UserRole = Application.Enums.Role.Admin.ToString()}
         ]);
-        
+
+        modelBuilder.Entity<Image>().HasData(images);
+        modelBuilder.Entity<Product>().HasData(products);
+
     }
 
     public DbSet<User> Users { get; set; }
